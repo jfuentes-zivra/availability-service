d1269ca3fb0d47e9c5a96ac70f7ab72afe2577e9
james <james@kapx.org>
2018-07-03 23:37:35 -0700

    hello-world!

---
+++ src/main/java/com/zivra/platform/AvailabilityService/AvailabilityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zivra.platform.AvailabilityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvailabilityServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zivra/platform/AvailabilityService/AvailabilityServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zivra.platform.AvailabilityService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvailabilityServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
