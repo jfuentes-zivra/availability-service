437c8ee211fd3f470cfac584836b111f2e6bba0a
james <james@kapx.org>
2018-09-06 14:55:38 -0700

    missing imports

--- src/main/java/com/zivra/platform/AvailabilityService/AvailabilityController.java
+++ src/main/java/com/zivra/platform/AvailabilityService/AvailabilityController.java
@@ -1,5 +1,11 @@
 package com.zivra.platform.AvailabilityService;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 
